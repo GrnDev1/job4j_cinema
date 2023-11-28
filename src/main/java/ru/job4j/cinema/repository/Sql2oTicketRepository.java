fb19655f540aa7ea4fbde30bd0ec72ce73967470
Roman <grndev@mail.ru>
2023-11-28 23:04:17 +0300

    Added repositories

---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oTicketRepository.java
@@ -0,0 +1,59 @@
+package ru.job4j.cinema.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.cinema.model.Ticket;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class Sql2oTicketRepository implements TicketRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(Sql2oUserRepository.class);
+    private final Sql2o sql2o;
+
+    public Sql2oTicketRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<Ticket> save(Ticket ticket) {
+        try (var connection = sql2o.open()) {
+            var sql = """
+                    INSERT INTO tickets(session_id, row_number, place_number, user_id)
+                    VALUES (:sessionId, :row, :place, :userId)
+                    """;
+            var query = connection.createQuery(sql, true)
+                    .addParameter("sessionId", ticket.getSessionId())
+                    .addParameter("row", ticket.getRow())
+                    .addParameter("place", ticket.getPlace())
+                    .addParameter("userId", ticket.getUserId());
+            int generatedId = query.executeUpdate().getKey(Integer.class);
+            ticket.setId(generatedId);
+            return Optional.of(ticket);
+        } catch (Exception e) {
+            LOG.info("Failed to purchase a ticket for a given seat.", e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Ticket> findById(int id) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM tickets WHERE id = :id");
+            query.addParameter("id", id);
+            var ticket = query.setColumnMappings(Ticket.COLUMN_MAPPING).executeAndFetchFirst(Ticket.class);
+            return Optional.ofNullable(ticket);
+        }
+    }
+
+    @Override
+    public List<Ticket> findAll() {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM halls");
+            return query.setColumnMappings(Ticket.COLUMN_MAPPING).executeAndFetch(Ticket.class);
+        }
+    }
+}
